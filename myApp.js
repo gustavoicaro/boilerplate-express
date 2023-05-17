9611b5fb95e9b6b6d68f5309c4c7e9fc1c71cf0a
Gustavo Ícaro <gustavoicaro+dev@pm.me>
2023-05-17 09:06:34 -0300

    Get data from POST requests

--- myApp.js
+++ myApp.js
@@ -35,9 +35,13 @@ app.get('/:word/echo', (req, res) => {
   res.json({ echo: req.params.word });
 });
 
-app.get('/name', (req, res) => {
-  res.json({ name: `${req.query.first} ${req.query.last}` });
-});
+app.route('/name')
+  .post((req, res) => {
+    res.json({ name: `${req.body.first} ${req.body.last}` });
+  })
+  .get((req, res) => {
+    res.json({ name: `${req.query.first} ${req.query.last}` });
+  });
 
 
 
