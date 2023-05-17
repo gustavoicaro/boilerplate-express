d372db83d92209fb24eb0151a6e74ec8f67ac50d
Gustavo Ícaro <gustavoicaro+dev@pm.me>
2023-05-17 08:49:10 -0300

    Add body-parser middleware

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
 
@@ -8,6 +8,8 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${__dirname}/public`));
 
@@ -36,7 +38,6 @@ app.get('/:word/echo', (req, res) => {
 app.get('/name', (req, res) => {
   res.json({ name: `${req.query.first} ${req.query.last}` });
 });
-
 
 
 
