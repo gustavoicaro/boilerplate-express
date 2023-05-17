7e0cf1bf6dcc5fa00e75b2c1114adb88ab195352
Gustavo Ícaro <gustavoicaro+dev@pm.me>
2023-05-17 03:26:10 -0300

    Add a simple logger middleware

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ let express = require('express');
 let app = express();
 
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 app.use('/public', express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
