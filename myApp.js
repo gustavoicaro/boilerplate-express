779f31cecafef6c17a0b0a5a337223dbb1c7e8b8
Gustavo Ícaro <gustavoicaro+dev@pm.me>
2023-05-17 03:43:43 -0300

    Add the `/now` route with time middleware

--- myApp.js
+++ myApp.js
@@ -22,7 +22,12 @@ app.get('/json', (req, res) => {
   res.json({ message });
 });
 
-
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({ time: req.time });
+});
 
 
 
