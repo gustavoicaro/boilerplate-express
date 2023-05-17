1c709db2635f3ff1d81f7e6e9f2aba07f6caa8c0
Gustavo Ícaro <gustavoicaro+dev@pm.me>
2023-05-17 01:52:04 -0300

    Send `index.html` as the response for the `/` path

--- myApp.js
+++ myApp.js
@@ -2,8 +2,8 @@ let express = require('express');
 let app = express();
 
 
-app.get('', (req, res) => {
-  res.send('Hello Express');
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`)
 });
 
 
